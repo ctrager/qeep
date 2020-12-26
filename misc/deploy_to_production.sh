fff383d0cd7268930f2a55a5eca5f16222c30e59
corey <ctrager@yahoo.com>
2020-12-25 22:10:35 -0600

    deployed

--- misc/deploy_to_production.sh
+++ misc/deploy_to_production.sh
@@ -7,7 +7,7 @@ git pull
 # so that we can look on the server and see what we are deploying
 git log -3 > git_log.txt
 dotnet publish -o ../qpublish_next
-cp qeep_config_active.txt ../qqpublish_next
+cp qeep_config_active.txt ../qpublish_next
 cp git_log.txt ../qpublish_next
 touch ../qpublish_next/$( date '+%Y-%m-%d_%H-%M-%S' )
 
